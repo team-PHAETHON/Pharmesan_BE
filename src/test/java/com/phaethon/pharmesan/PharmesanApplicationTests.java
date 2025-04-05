c6b141227abdde2b2a9b612e1d13314a225d103b
rejo <readytojoin086@gmail.com>
2025-04-05 10:30:21 +0000

    feat: Add Project Files

---
+++ src/test/java/com/phaethon/pharmesan/PharmesanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaethon.pharmesan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmesanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
