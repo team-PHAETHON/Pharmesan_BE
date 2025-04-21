8fcd13de35edc048edb0ac467fc6746bb1397d2d
r3j0 <readytojoin086@gmail.com>
2025-04-21 13:59:29 +0900

    chore: add comments in DrugSearchService.java

--- src/main/java/com/phaethon/pharmesan/Service/DrugSearchService.java
+++ src/main/java/com/phaethon/pharmesan/Service/DrugSearchService.java
@@ -9,17 +9,31 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 import java.util.List;
 
-@Service
-@Transactional
-public class DrugSearchService {
-    @Autowired
+@Service // 이 클래스가 서비스 클래스임.
+@Transactional // 이 클래스 내의 모든 메소드가 트랜잭션 관리 대상임.
+public class DrugSearchService { 
+
+    @Autowired // SQLiteDrugRepository 의존성 주입
     private SQLiteDrugRepository sqLiteDrugRepository;
 
-    // TODO : Log 기능
+    // TODO : 의약외품 검색 기능 관련 메소드에 대한 로그 기록 기능 추가 필요
+
+    /**
+     * public Drug addDrug(Drug)
+     *   Description : 새로운 약품 정보를 데이터베이스에 저장하는 메소드
+     *   Parameter   : - Drug drug          저장할 약품 정보
+     *   Return      : - Drug               저장한 약품 정보를 반환
+     */
     public Drug addDrug(Drug drug) {
         return sqLiteDrugRepository.save(drug);
     }
 
+    /**
+     * public List<Drug> getDrugsByItemName(String)
+     *   Description : 제품명을 포함한 약품을 데이터베이스에서 조회하는 메소드
+     *   Parameter   : - String itemName    제품명
+     *   Return      : List<Drug>           제품명을 포함한 약품 리스트
+     */
     public List<Drug> getDrugsByItemName(String itemName) {
         return sqLiteDrugRepository.findByItemNameContaining(itemName);
     }
