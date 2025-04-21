9f90ba5274bed10fff4f82c7ef0853b71fc889a6
r3j0 <readytojoin086@gmail.com>
2025-04-21 13:50:41 +0900

    refactor: update Column information and add comments in Drug.java

--- src/main/java/com/phaethon/pharmesan/Entity/Drug.java
+++ src/main/java/com/phaethon/pharmesan/Entity/Drug.java
@@ -7,91 +7,93 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Getter
+@Getter // Lombok : Getter, Setter 자동 생성
 @Setter
-@Entity
-@Table(name = "drug_details")
+@Entity                         // 이 클래스가 JPA 엔티티임.
+@Table(name = "drug_details")   // 이 클래스가 'drug_details' 테이블에 매핑됨
 public class Drug {
     @Id
-    @Column(name = "id")
-    private long id;
+    @Column(name = "_id")
+    private String id; // 도큐먼트 아이디
 
-    @Column(name = "entp_name")
-    private String entpName;
+    @Column(name = "ITEM_SEQ")
+    private String itemSeqI; // 품목일련번호 - FK(Identification)
+
+    @Column(name = "itemSeq")
+    private String itemSeqE; // 품목기준코드 - FK(easyInfo)
+
+    @Column(name = "enterprise_name")
+    private String enterpriseName; // 업체명
 
     @Column(name = "item_name")
-    private String itemName;
+    private String itemName; // 제품명
 
-    @Column(name = "item_seq")
-    private String itemSeq;
+    @Column(name = "efficacy")
+    private String efficacy; // 효능
 
-    @Column(name = "efcy_qesitm")
-    private String efcyQesitm;
+    @Column(name = "method")
+    private String method;  // 사용법
 
-    @Column(name = "use_method_qesitm")
-    private String useMethodQesitm;
+    @Column(name = "warning")
+    private String warning;  // 주의사항경고
 
-    @Column(name = "atpn_warn_qesitm")
-    private String atpnWarnQesitm;
+    @Column(name = "attention")
+    private String attention;  // 주의사항
 
-    @Column(name = "atpn_qesitm")
-    private String atpnQesitm;
+    @Column(name = "interaction")
+    private String interaction;  // 상호작용
 
-    @Column(name = "intrc_qesitm")
-    private String intrcQesitm;
-
-    @Column(name = "se_qesitm")
-    private String seQesitm;
+    @Column(name = "side_effect")
+    private String sideEffect;  // 부작용
 
     @Column(name = "item_image")
-    private String itemImage;
+    private String itemImage;  // 낱알 이미지
 
     @Column(name = "chart")
-    private String chart;
+    private String chart;  // 성상
 
     @Column(name = "print_front")
-    private String printFront;
+    private String printFront;  // 표시(앞)
 
     @Column(name = "print_back")
-    private String printBack;
+    private String printBack;  // 표시(뒤)
 
     @Column(name = "drug_shape")
-    private String drugShape;
+    private String drugShape;  // 의약품모양
 
-    @Column(name = "color_class1")
-    private String colorClass1;
+    @Column(name = "color_front")
+    private String colorFront;  // 색깔(앞)
 
-    @Column(name = "color_class2")
-    private String colorClass2;
+    @Column(name = "color_back")
+    private String colorBack;  // 색깔(뒤)
 
     @Column(name = "line_front")
-    private String lineFront;
+    private String lineFront;  // 분할선(앞)
 
     @Column(name = "line_back")
-    private String lineBack;
+    private String lineBack;  // 분할선(뒤)
 
-    @Column(name = "leng_long")
-    private double lengLong;
+    @Column(name = "line_long")
+    private double lineLong;  // 크기(장축)
 
-    @Column(name = "leng_short")
-    private double lengShort;
+    @Column(name = "line_short")
+    private double lineShort;  // 크기(단축)
 
     @Column(name = "thick")
-    private double thick;
+    private double thick;  // 크기(두께)
 
     @Column(name = "class_no")
-    private String classNo;
+    private String classNo;  // 분류번호
 
     @Column(name = "class_name")
-    private String className;
+    private String className;  // 분류명
 
-    @Column(name = "etc_otc_name")
-    private String etcOtcName;
+    @Column(name = "etc_otc")
+    private String etcOtc;  // 전문/일반
 
     @Column(name = "form_code_name")
-    private String formCodeName;
+    private String formCodeName;  // 제형코드이름
 
     @Column(name = "item_eng_name")
-    private String itemEngName;
-
+    private String itemEngName;  // 제품영문명
 }
