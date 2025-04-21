f847e89bec7fc98df85dad07c49c5449a835ad3e
r3j0 <readytojoin086@gmail.com>
2025-04-21 13:34:27 +0900

    chore: remove unnecessary annotations from PharmesanApplication.java and add comments

--- src/main/java/com/phaethon/pharmesan/PharmesanApplication.java
+++ src/main/java/com/phaethon/pharmesan/PharmesanApplication.java
@@ -2,12 +2,15 @@ package com.phaethon.pharmesan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.phaethon.pharmesan.Repository.SQLite")  // 리포지토리 경로
+@SpringBootApplication // Spring Boot 애플리케이션을 시작하는 주요 어노테이션
+// 이 어노테이션은 @Configuration, @EnableAutoConfiguration, @ComponentScan을 포함하며, 
+// 자동 설정 및 컴포넌트 검색을 수행하여 애플리케이션을 초기화합니다.
+
 public class PharmesanApplication {
 	public static void main(String[] args) {
+		// SpringApplication.run() 메서드를 통해 Spring Boot 애플리케이션을 실행
+		// PharmesanApplication 클래스를 부트스트랩 클래스로 지정하고, 애플리케이션을 실행시킴
 		SpringApplication.run(PharmesanApplication.class, args);
 	}
 }
